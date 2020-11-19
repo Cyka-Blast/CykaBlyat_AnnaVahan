07c036194b4953337ba2ea1a7558c6947061d8a3
RudrakshK <stark.rudraksh@gmail.com>
2020-11-20 03:05:46 +0530

    Business details db finished.

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from flask_sqlalchemy.model import camel_to_snake_case
 
 from marshmallow.fields import Method
 
@@ -20,31 +21,52 @@ db = SQLAlchemy(app)   #lol db looks like dick
 #Init marshmallow ma
 ma = Marshmallow(app)
 
+
 #Init food class and model
 class food(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(200))
-    price = db.Column(db.Float)
+    price = db.Column(db.Integer)
 
     def __init__(self, name , price):
         self.name = name
         self.price = price
-        
+    
+
+
+#Init business class and model
+class business(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200))
+    address = db.Column(db.String(200))
+    latitude = db.Column(db.String(20))
+    longitude = db.Column(db.String(20))
+
+
 
 #Schema
 class foodSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'price')
 
+class businessSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'address', 'latitude', 'longitude')
+
+
 #Init Schema
 food_schema = foodSchema()
 foods_schema = foodSchema(many = True)
+
+business_Schema = businessSchema()
+morebusiness_Schema = businessSchema(many = True)
+
 
 #Input food item
 @app.route('/food', methods=['POST'])
 def add_food():
     name = request.json["name"]
-    price = request.json["price"]
+    price = int(request.json["price"])*100
 
     print(name, price)
     
@@ -56,11 +78,36 @@ def add_food():
     return food_schema.jsonify(nfood)
 
 
-#Fetch a list of products
-@app.route('/getfood', methods=['GET'])
+#Fetch a list of foods
+@app.route('/food', methods=['GET'])
 def get_food():
     allFoods = food.query.all()
     result = foods_schema.dump(allFoods)
+    return jsonify(result)
+
+
+#Input business
+@app.route('/business', methods=['POST'])
+def add_business():
+    name = request.json["name"]
+    address = request.json["address"]
+    latitude = request.json["latitude"]
+    longitude = request.json["longitude"]
+
+    
+    nbus = business(name = name, address = address, latitude = latitude, longitude = longitude)
+
+    db.session.add(nbus)
+    db.session.commit()
+
+    return business_Schema.jsonify(nbus)
+
+
+#Fetch a list of businesses
+@app.route('/business', methods=['GET'])
+def get_business():
+    allbusiness = business.query.all()
+    result = morebusiness_Schema.dump(allbusiness)
     return jsonify(result)
 
 @app.route('/', methods=['GET'])
